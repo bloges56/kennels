d7e5b1440ee76b238af7ff7c6ac797a733976ff8
Brady Logan <brady.drew.logan@gmail.com>
2020-10-02 13:44:25 -0500

    can now navigate between all components

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,9 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalCard } from "./animal/AnimalCard"
+import { LocationCard } from "./location/Location"
+import { EmployeeCard } from "./employee/EmployeeCard"
+import { CustomerCard } from "./customer/CustomerCard"
 
 export const ApplicationViews = (props) => {
     return (
@@ -13,9 +16,33 @@ export const ApplicationViews = (props) => {
 
             {/* Render the animal list when http://localhost:3000/animals */}
             <Route path="/animals">
-                <div class="animals">
+                <div className="animals">
                     <AnimalCard />
                     <AnimalCard />
+                </div>
+            </Route>
+
+            {/* Render the location list when http://localhost:3000/locations */}
+            <Route path="/locations">
+                <div className="locations">
+                    <LocationCard />
+                    <LocationCard />
+                </div>
+            </Route>
+
+            {/* Render the employee list when http://localhost:3000/employees */}
+            <Route path="/employees">
+                <div className="employees">
+                    <EmployeeCard />
+                    <EmployeeCard />
+                </div>
+            </Route>
+
+            {/* Render the customer list when http://localhost:3000/customers */}
+            <Route path="/customers">
+                <div className="customers">
+                    <CustomerCard />
+                    <CustomerCard />
                 </div>
             </Route>
         </>
