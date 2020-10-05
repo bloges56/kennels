f2ea16d87948fb252e76bca0eee7af95dd6dba25
Brady Logan <brady.drew.logan@gmail.com>
2020-10-05 15:00:23 -0500

    created animal form component where the user can add animals to the database

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
+import { useHistory } from "react-router-dom"
 
 export const AnimalList = () => {
    // This state changes when `getAnimals()` is invoked below
@@ -14,15 +15,19 @@ export const AnimalList = () => {
 		
     }, [])
 
-
-    return (	
-		<div className="animals">
-		    {console.log("AnimalList: Render")}
-        {
-			animals.map(animal => {
-				return <AnimalCard key={animal.id} location={animal.location.name} animal={animal} />
-			})
-        }
+    const history = useHistory()
+    return (
+      <>
+        <h2>Animals</h2>
+        <button onClick={() => {history.push("/animals/create")}}>Add Animal</button>	
+        <div className="animals">
+          {console.log("AnimalList: Render")}
+          {
+            animals.map(animal => {
+              return <AnimalCard key={animal.id} location={animal.location.name} animal={animal} />
+            })
+          }
         </div>
+        </>
     )
 }
